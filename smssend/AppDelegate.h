82e25879bc19345b7043b9c45cb5c76b9fd64536
511308538@qq.com <511308538@qq.com>
2017-03-07 16:36:54 +0800

    Initial Commit

---
+++ smssend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  smssend
+//
+//  Created by hennychen on 3/7/17.
+//  Copyright © 2017 hennychen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
